30b0ed7e55f43fed81b1e486d4dddf092a75b463
Uros <prijovic.uros13@gmail.com>
2023-05-28 16:17:28 +0200

    Rules

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -1,31 +1,26 @@
 package com.ftn.sbnz.service;
 
-import java.util.Arrays;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+@SpringBootApplication(scanBasePackages = {"com.ftn.sbnz.model", "com.ftn.sbnz.kjar"})
+public class ServiceApplication {
 
-@SpringBootApplication
-public class ServiceApplication  {
-	
-	public static void main(String[] args) {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
 
-	@Bean
-	public KieContainer kieContainer() {
-		KieServices ks = KieServices.Factory.get();
-		KieContainer kContainer = ks
-				.newKieContainer(ks.newReleaseId("com.ftn.sbnz", "kjar", "0.0.1-SNAPSHOT"));
-		KieScanner kScanner = ks.newKieScanner(kContainer);
-		kScanner.start(1000);
-		return kContainer;
-	}
+    @Bean
+    public KieContainer kieContainer() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks
+                .newKieContainer(ks.newReleaseId("com.ftn.sbnz", "kjar", "0.0.1-SNAPSHOT"));
+        KieScanner kScanner = ks.newKieScanner(kContainer);
+        kScanner.start(10_000);
+        return kContainer;
+    }
 }
