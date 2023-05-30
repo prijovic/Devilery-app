44275fdf57a800c6ff9a3585d0dd4a56b2975412
Uros <prijovic.uros13@gmail.com>
2023-05-30 13:49:51 +0200

    Menu Item Deletion

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controller/MenuItemController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controller/MenuItemController.java
@@ -1,8 +1,10 @@
 package com.ftn.sbnz.service.controller;
 
+import com.ftn.sbnz.model.models.MenuItem;
 import com.ftn.sbnz.service.dto.request.menuItem.MenuItemUpdateRequest;
 import com.ftn.sbnz.service.dto.request.menuItem.NewMenuItemRequest;
 import com.ftn.sbnz.service.services.menuItem.CreateMenuItem;
+import com.ftn.sbnz.service.services.menuItem.DeleteMenuItem;
 import com.ftn.sbnz.service.services.menuItem.UpdateMenuItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.UUID;
 public class MenuItemController {
     private final CreateMenuItem createMenuItem;
     private final UpdateMenuItem updateMenuItem;
+    private final DeleteMenuItem deleteMenuItem;
 
     @PostMapping
     public void createMenuItem(@RequestBody NewMenuItemRequest newMenuItemRequest) {
@@ -23,11 +26,12 @@ public class MenuItemController {
     }
 
     @PutMapping("/{id}")
-    public void updateMenuItem(@NotBlank @PathVariable UUID id, @RequestBody MenuItemUpdateRequest menuItemUpdateRequest) {
-        updateMenuItem.execute(id, menuItemUpdateRequest);
+    public MenuItem updateMenuItem(@NotBlank @PathVariable UUID id, @RequestBody MenuItemUpdateRequest menuItemUpdateRequest) {
+        return updateMenuItem.execute(id, menuItemUpdateRequest);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMenuItem(@NotBlank @PathVariable UUID id) {
+        deleteMenuItem.execute(id);
     }
 }
