a2ecb0ca84c5eb8e003033b224db2ef251ae10ab
Uros <prijovic.uros13@gmail.com>
2023-05-31 10:18:28 +0200

    Logout

--- frontend/src/app/components/navbar/user-profile-button/user-profile-button.component.ts
+++ frontend/src/app/components/navbar/user-profile-button/user-profile-button.component.ts
@@ -2,6 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {SafeUrl} from "@angular/platform-browser";
 import {DOCUMENT} from "@angular/common";
 import {PictureService} from "../../../services/picture.service";
+import {Store} from "@ngrx/store";
+import {logout} from "../../../auth/store/auth.actions";
 
 @Component({
   selector: 'app-user-profile-button',
@@ -11,11 +13,11 @@ import {PictureService} from "../../../services/picture.service";
 export class UserProfileButtonComponent implements OnInit {
   image!: SafeUrl;
 
-  constructor(@Inject(DOCUMENT) private document: Document, private pictureService: PictureService) {}
+  constructor(@Inject(DOCUMENT) private document: Document, private pictureService: PictureService, private store: Store) {}
 
   ngOnInit(): void {
     this.pictureService.getSelfProfilePicture().subscribe(profilePicture => {
-      if (document.defaultView) {
+      if (document.defaultView && profilePicture.size > 0) {
         this.image = document.defaultView.URL.createObjectURL(profilePicture);
       }
   }
@@ -24,5 +26,9 @@ export class UserProfileButtonComponent implements OnInit {
 
   getBackgroundImageUrl() {
     return `url("${this.image}")`;
+  }
+
+  logout() {
+    this.store.dispatch(logout());
   }
 }
